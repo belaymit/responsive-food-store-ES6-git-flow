bcfc032254c95e23229c3445ba6ab45c72ac8d4f
belaymit <2belamit@gmail.com>
2023-01-07 11:54:54 +0300

    added login form variables to constants

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -2,3 +2,5 @@ export const searchForm = document.querySelector('.search-form');
 export const searchBtn = document.querySelector('#search-btn');
 export const shoppingCart = document.querySelector('.shopping-cart');
 export const cartBtn = document.querySelector('#cart-btn');
+export const loginForm = document.querySelector('.login-form');
+export const loginBtn = document.querySelector('#login-btn');
