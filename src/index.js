94eaabb22fcc318cced283c3a55c7e8915ad0ba4
belaymit <2belamit@gmail.com>
2023-01-08 11:09:36 +0300

    fixed linter error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import showSearchBar from './modules/showSearchBar.js';
 import showLogin from './modules/showLogin.js';
 import showCart from './modules/showCart.js';
 import showMenu from './modules/showMenu.js';
-import swiperHandler from './modules/swiperHandler';
+import swiperHandler from './modules/swiperHandler.js';
 
 showSearchBar();
 showCart();
