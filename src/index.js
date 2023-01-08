b82f0a0c26979f9aa510ae8d998b888157d61efb
belaymit <2belamit@gmail.com>
2023-01-08 10:06:51 +0300

    imported swiper function

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import showSearchBar from './modules/showSearchBar.js';
 import showLogin from './modules/showLogin.js';
 import showCart from './modules/showCart.js';
 import showMenu from './modules/showMenu.js';
+import swiperHandler from './modules/swiperHandler';
 
 showSearchBar();
 showCart();
 showLogin();
 showMenu();
+swiperHandler();
